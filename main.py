c41fddfe1daa4eef0db05436f1526773c10c7a33
Sahat Renaldi S <renaldi.crk@gmail.com>
2025-12-26 14:29:10 +0700

    Fix deploy: move SCHEMES out of config

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import datetime as dt
 
-from config import SCHEMES
+# from config import SCHEMES
+from schemes import SCHEMES
 from queries import SIMULATION_QUERY
 from db import fetch_dataframe
 
